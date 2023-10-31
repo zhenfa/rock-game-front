206f33a72ee118a3e0882f638077435ada89b74c
zhenfa <oveu123@gmail.com>
2023-10-31 10:14:01 +0800

    [feat]-加入路由匹配 加入匹配登入、選擇角色、主畫面、404路由匹配

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import LoginLayout from './pages/LoginLayout';
+import CharatorPickPage from './pages/CharatorPickPage';
+import MainLayout from './pages/MainLayout';
 
 function App() {
   return (
-    <LoginLayout />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" /> } />
+        <Route path="/login" element={<LoginLayout /> } />
+        <Route path="/pick" element={<CharatorPickPage /> } />
+        <Route path="/main" element={ <MainLayout /> } />
+        <Route path="*" element={<h1 className='text-center text-lg font-bold'>Error 404, Page not found</h1> } />
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
